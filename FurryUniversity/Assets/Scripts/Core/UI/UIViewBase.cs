d68127ddafb17a9364a9a9d6c0a16cd342aab82b
Summid <summid@foxmail.com>
2022-12-30 17:17:47 +0800

    UIManager核心逻辑更新，有什么东西烧起来了（意思是猪脑过载）

--- FurryUniversity/Assets/Scripts/Core/UI/UIViewBase.cs
+++ FurryUniversity/Assets/Scripts/Core/UI/UIViewBase.cs
@@ -1,3 +1,4 @@
+using SFramework.Core.GameManagers;
 using SFramework.Threading.Tasks;
 using System;
 using System.Collections;
@@ -10,6 +11,16 @@ namespace SFramework.Core.UI
     {
         public EnumUIType UIType { get; set; }
 
+        public delegate void PageHandleDelVoid(Type type);
+        public delegate bool PageHandleDelBool(Type type);
+
+        public PageHandleDelVoid SetPageEnable;
+        public PageHandleDelVoid SetPageShow;
+        public PageHandleDelVoid SetUIActive;
+        public PageHandleDelBool SetPageHide;
+        public PageHandleDelVoid SetUIDisactive;
+        public PageHandleDelVoid RemoveUI;
+
         private RectTransform visualRoot;
         private CanvasGroup rootCanvas;
 
@@ -18,6 +29,20 @@ namespace SFramework.Core.UI
         public virtual bool Topmost => false;
 
         protected override RectTransform VisualRoot => this.visualRoot;
+
+        private string basedPage;
+        /// <summary> 依附的Page的名称 </summary>
+        public virtual string BasedPage
+        {
+            get => this.basedPage;
+            set
+            {
+                if (this.basedPage == value)
+                    return;
+                this.basedPage = value;
+                this.CalWindowShow();//判断下自己是否应该显示
+            }
+        }
 
         public sealed override async void Awake(GameObject gameObjectHost)
         {
@@ -81,6 +106,178 @@ namespace SFramework.Core.UI
             if (this.UIType == EnumUIType.Window)
                 this.Hide();
         }
+
+        /// <summary>
+        /// 显示UI
+        /// </summary>
+        public sealed override void Show()
+        {
+            if (this.UIType == EnumUIType.Page)
+                this.SetPageEnable?.Invoke(this.ClassType);
+
+            if (this.UIState == EnumViewState.Shown)
+                return;
+            if(this.UIState == EnumViewState.Hidding)
+                this.OnDisable();
+
+            this.Mask.gameObject.SetActive(true);
+            this.Mask.EnableRaycast = false;
+            this.UIState = EnumViewState.Shown;
+            if (this.UIType == EnumUIType.Page)
+                this.SetPageShow?.Invoke(this.ClassType);
+            else
+            {
+                this.SetUIActive?.Invoke(this.ClassType);
+            }
+            this.OnWillShow();
+            this.Mask.EnableRaycast = true;
+            this.OnShow();
+        }
+
+        /// <summary>
+        /// 隐藏UI
+        /// </summary>
+        public override void Hide()
+        {
+            if (this.UIState == EnumViewState.Disposed)
+            {
+                Debug.LogWarning($"{this} Hide请求失败，物体已被销毁");
+                return;
+            }
+            if (this.UIState == EnumViewState.Hidden)
+                return;
+
+            if (this.UIType == EnumUIType.Page)
+            {
+                if (!GameManager.Instance.UIManager.IsLastPage(this))
+                {
+                    this.UIState = EnumViewState.Hidding;
+
+                    if (!GameManager.Instance.UIManager.InternalHanding)
+                        this.OnWillHide();
+
+                    //Hide流程被取消
+                    if (this.UIState != EnumViewState.Hidding)
+                        return;
+
+                    this.SetPageHide?.Invoke(this.ClassType);
+                }
+                else
+                {
+                    Debug.LogWarning($"{this} Dispose请求失败，限制唯一的Page Hide");
+                }
+            }
+            else
+            {
+                this.UIState = EnumViewState.Hidding;
+                if (!GameManager.Instance.UIManager.InternalHanding)
+                    this.OnWillHide();
+
+                if (this.UIState != EnumViewState.Hidding)
+                    return;
+
+                this.SetUIDisactive?.Invoke(this.ClassType);
+                this.UIState = EnumViewState.Hidden;
+                this.OnDisable();
+            }
+            if (this.Mask.gameObject != null && this.UIState != EnumViewState.Shown)
+                this.Mask.gameObject.SetActive(false);
+            GameManager.Instance.UIManager.UpdateUIInsteanceLimit();
+        }
+
+        public sealed override void Dispose()
+        {
+            try
+            {
+                if (this.UIType == EnumUIType.Page && this.UIState == EnumViewState.Shown)
+                    this.SetPageHide?.Invoke(this.ClassType);
+
+                this.RemoveUI?.Invoke(this.ClassType);
+
+                if(this.UIState != EnumViewState.Hidden)
+                    this.OnDisable();
+
+                base.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+        }
+
+        /// <summary>
+        /// 调用 <see cref="Show"/> 时触发，可在此处理View弹出动画等逻辑
+        /// </summary>
+        protected virtual void OnWillShow()
+        {
+
+        }
+
+        /// <summary>
+        /// 调用 <see cref="Hide"/> 时触发，可再此处理View关闭动画等逻辑
+        /// </summary>
+        protected virtual void OnWillHide()
+        {
+
+        }
+
+        /// <summary>
+        /// 若当前依附的Page没有显示时，自己也要隐藏
+        /// </summary>
+        public void CalWindowShow()
+        {
+            if (this.UIType == EnumUIType.Page)
+                return;
+
+            if (string.IsNullOrEmpty(this.BasedPage))
+                return;
+
+            if (this.BasedPage != GameManager.Instance.UIManager.GetCurrentPageName())
+            {
+                this.rootCanvas.alpha = 0;
+                this.rootCanvas.blocksRaycasts = false;
+            }
+            else
+            {
+                this.rootCanvas.alpha = 1;
+                this.rootCanvas.blocksRaycasts = true;
+            }
+        }
         #endregion
+        
+        /// <summary>
+        /// just set state show state without external logic
+        /// </summary>
+        internal void SetStateShow()
+        {
+            if(this.UIState != EnumViewState.Shown)
+                this.UIState = EnumViewState.Shown;
+            this.OnShow();
+        }
+
+        /// <summary>
+        /// just set state hide state without external logic
+        /// </summary>
+        internal void SetStateHide()
+        {
+            if (this.UIState == EnumViewState.Hidding)
+                return;
+
+            this.UIState = EnumViewState.Hidding;
+            this.OnWillHide();
+
+            if (this.UIState != EnumViewState.Hidding)
+                return;
+            this.UIState = EnumViewState.Hidden;
+            this.OnDisable();
+        }
+
+        /// <summary>
+        /// 调用Hide后触发，底层使用不暴露给用户
+        /// </summary>
+        protected sealed override void OnDisable()
+        {
+            base.OnDisable();
+        }
     }
 }
--- FurryUniversity/Assets/Scripts/GameManagers/UIManager.cs
+++ FurryUniversity/Assets/Scripts/GameManagers/UIManager.cs
@@ -4,6 +4,7 @@ using SFramework.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace SFramework.Core.GameManagers
@@ -19,13 +20,26 @@ namespace SFramework.Core.GameManagers
         private bool isInited;
         private UIInfoList uiList;
         private Dictionary<Type,UIInstanceInfo> uiInstances = new Dictionary<Type,UIInstanceInfo>();//加载后的View保存在这
-        private List<UIInstanceInfo> uiInstanceLoadOrderList = new List<UIInstanceInfo>();//根据View显示顺序排序
+        private List<UIInstanceInfo> uiInstanceLoadOrderList = new List<UIInstanceInfo>();//根据View显示顺序排序，越后显示的排在越后
+        private List<Type> navigateQueue = new List<Type>();//Page页面跳转顺序记录
         private Transform uiRoot;
         private CanvasGroup canvasGroup;
         private RectTransform uiWindowRoot;
         private RectTransform uiTopWindowRoot;
 
         public Canvas UIRootCanvas { get; private set; }
+        /// <summary> 当统一处理UI时，若涉及到修改uiInstances等成员，通过该标志位判断处理状态 </summary>
+        public bool InternalHanding { get; private set; }
+        private int uiInstanceCacheLimitCount = int.MaxValue;
+        public int UIInstanceCacheLimitCount
+        {
+            get => this.uiInstanceCacheLimitCount;
+            private set
+            {
+                this.uiInstanceCacheLimitCount = value;
+                this.UpdateUIInsteanceLimit();
+            }
+        }
 
         protected override async void OnInitialized()
         {
@@ -52,12 +66,23 @@ namespace SFramework.Core.GameManagers
 
             this.isInited = true;
 
+            this.UIInstanceCacheLimitCount = 5;//暂定5个缓存
+
             Debug.Log("UIManager Initialized");
 
             //TODO 刘海屏设备清单加载
         }
 
         #region 外部接口
+        public async STask<ViewInstance> ShowUIAsync<ViewInstance>() where ViewInstance : UIViewBase, new()
+        {
+            if (!this.isInited)
+                return null;
+
+            Type viewType = typeof(ViewInstance);
+            return await this.ShowUIInternalAsync(viewType) as ViewInstance;
+        }
+
         public UIViewInfo GetUIViewInfo(Type uiObjectType)
         {
             string targetTypeName = uiObjectType.FullName;
@@ -112,12 +137,91 @@ namespace SFramework.Core.GameManagers
 
             rect.localScale = Vector3.one;
         }
+
+        /// <summary>
+        /// Page导航队列是否已经无法后退（队列中不超过一个Page）
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public bool IsLastPage(UIViewBase page)
+        {
+            int index = this.navigateQueue.IndexOf(page.ClassType);
+            return index == 0;
+        }
+
+        public void UpdateUIInsteanceLimit()
+        {
+            if (this.InternalHanding)
+                return;
+
+            try
+            {
+                //拷贝一份处于Hidden状态的View
+                List<UIInstanceInfo> cacheList = this.uiInstanceLoadOrderList.Where(info => info.ViewInstance.UIState == UIObject.EnumViewState.Hidden).ToList();
+                int needRemoveCount = cacheList.Count - this.UIInstanceCacheLimitCount;
+                int index = 0;
+                this.InternalHanding = true;
+                while (needRemoveCount > 0)
+                {
+                    var uiInfoNeedRemove = cacheList[index];
+                    Debug.Log($"<color=#00ff00>超出UI缓存最大限制:{UIInstanceCacheLimitCount}，移除UI实例:[{uiInfoNeedRemove.ViewName}]</color>");
+                    uiInfoNeedRemove.ViewInstance.Dispose();
+                    needRemoveCount--;
+                    index++;
+                }
+            }
+            finally
+            {
+                this.InternalHanding = false;
+            }
+        }
+
+        /// <summary>
+        /// 获取Page导航队列中最后一个Page对象名
+        /// </summary>
+        /// <returns></returns>
+        public string GetCurrentPageName()
+        {
+            if (this.navigateQueue.Count == 0)
+                return null;
+
+            Type type = this.navigateQueue[navigateQueue.Count - 1];
+            return type.Name;
+        }
         #endregion
 
         #region 内部方法
-        private UIViewBase ShowUI(Type viewType)
+        private async STask<UIViewBase> ShowUIInternalAsync(Type viewType)
         {
-            return default;
+            UIViewBase view = null;
+
+            if (this.uiInstances.TryGetValue(viewType, out UIInstanceInfo uiInfo))
+            {
+                view = uiInfo.ViewInstance;
+                //当一个界面Show的时候，将该界面在队列中的顺序提高到最后
+                this.uiInstanceLoadOrderList.Remove(uiInfo);
+                this.uiInstanceLoadOrderList.Add(uiInfo);
+            }
+            else
+            {
+                UIViewInfo targetUIInfo = this.GetUIViewInfo(viewType);
+
+                if (targetUIInfo != null)
+                {
+                    if (targetUIInfo.ViewAssetBundleName != null && targetUIInfo.ViewAssetName != null)
+                    {
+                        view = await this.InitViewAsync(targetUIInfo.ViewAssetBundleName, targetUIInfo.ViewAssetName, viewType, targetUIInfo.ViewType);
+                    }
+                }
+            }
+            if (view.UIType == EnumUIType.Window && this.navigateQueue.Count > 0)
+            {
+                view.BasedPage = this.navigateQueue[this.navigateQueue.Count - 1].Name;
+            }
+            this.uiInstances[viewType].ViewInstance.gameObject.transform.SetAsFirstSibling();
+            view.Show();
+
+            return view;
         }
 
         private async STask<UIViewBase> InitViewAsync(string viewBundleName, string viewPrefabName, Type viewType, EnumUIType uiType)
@@ -142,7 +246,175 @@ namespace SFramework.Core.GameManagers
 
         private void SetViewUtilityFunc(UIViewBase view)
         {
+            view.SetPageEnable = this.SetPageEnable;
+            view.SetPageShow = this.SetPageShow;
+            view.SetPageHide = this.SetPageHide;
+            view.SetUIActive = this.SetUIActive;
+            view.SetUIDisactive = this.SetUIDisactive;
+            view.RemoveUI = this.RemoveUI;
+        }
 
+        /// <summary>
+        /// 释放UI资源，并销毁相关GameObject（销毁相关逻辑在UIObject.Dispose中）
+        /// </summary>
+        /// <param name="viewType"></param>
+        private void RemoveUI(Type viewType)
+        {
+            if (this.uiInstances.TryGetValue(viewType, out UIInstanceInfo uiInfo))
+            {
+                this.uiInstances.Remove(viewType);
+                this.uiInstanceLoadOrderList.Remove(uiInfo);
+            }
+        }
+
+        /// <summary>
+        /// set window view active false
+        /// </summary>
+        /// <param name="viewType"></param>
+        private void SetUIDisactive(Type viewType)
+        {
+            if (this.uiInstances.TryGetValue(viewType, out UIInstanceInfo uiInfo))
+            {
+                uiInfo.ViewInstance.gameObject.SetActive(false);
+            }
+        }
+
+        /// <summary>
+        /// set window view active true
+        /// </summary>
+        /// <param name="viewType"></param>
+        private void SetUIActive(Type viewType)
+        {
+            if (this.uiInstances.TryGetValue(viewType, out UIInstanceInfo uiInfo))
+            {
+                uiInfo.ViewInstance.gameObject.SetActive(true);
+            }
+        }
+
+        /// <summary>
+        /// 设置Page View隐藏，上一个Page自动显示
+        /// </summary>
+        /// <param name="viewType"></param>
+        /// <returns></returns>
+        private bool SetPageHide(Type viewType)
+        {
+            if (this.InternalHanding)
+            {
+                var willHidePage = this.uiInstances[viewType];
+                willHidePage.ViewInstance.gameObject.SetActive(false);
+                return true;
+            }
+
+            //有上一个Page才设置
+            if (this.navigateQueue.Count >= 2)
+            {
+                var willHidePage = this.uiInstances[viewType];
+                willHidePage.ViewInstance.gameObject.SetActive(false);
+                willHidePage.ViewInstance.SetStateHide();
+
+                this.navigateQueue.RemoveAt(this.navigateQueue.Count - 1);
+
+                Type preType = this.navigateQueue[this.navigateQueue.Count - 1];
+                if (!this.uiInstances.ContainsKey(preType))
+                {
+                    //上一个Page被清理掉了，重新加载
+                    this.ShowUIInternalAsync(preType).Forget();
+                }
+                else
+                {
+                    //缓存中有上一个Page，直接显示
+                    var preUIInstance = this.uiInstances[this.navigateQueue[this.navigateQueue.Count - 1]];
+                    preUIInstance.ViewInstance.gameObject.SetActive(true);
+                    preUIInstance.ViewInstance.SetStateShow();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 设置Page View显示，上一个Page隐藏
+        /// </summary>
+        /// <param name="viewType"></param>
+        private void SetPageShow(Type viewType)
+        {
+            var viewInstance = this.uiInstances[viewType].ViewInstance;
+            if (this.InternalHanding)
+            {
+                viewInstance.gameObject.SetActive(true);
+                return;
+            }
+
+            //隐藏上一个Page
+            for (int i = 0, count = this.navigateQueue.Count; i < count; i++)
+            {
+                Type type = this.navigateQueue[i];
+                if (type == viewType)
+                    continue;
+                if (this.uiInstances.TryGetValue(type, out UIInstanceInfo uIInstanceInfo))
+                {
+                    uIInstanceInfo.ViewInstance.gameObject.SetActive(false);
+                    uIInstanceInfo.ViewInstance.SetStateHide();
+                }
+            }
+
+            //整理导航队列
+            if (!this.navigateQueue.Contains(viewType))
+            {
+                //显示的是新Page，将新Page加入导航队列
+                if (this.navigateQueue.Count > 0)
+                {
+                    //如果前一个Page是IsDisableNavigatePage，则把该Page移除导航队列
+                    Type prePageType = this.navigateQueue[this.navigateQueue.Count - 1];
+                    if (this.GetUIViewInfo(prePageType).IsDisableNavigatePage)
+                    {
+                        this.navigateQueue.Remove(prePageType);
+                    }
+                }
+                this.navigateQueue.Add(viewType);
+            }
+            else
+            {
+                //如果显示的Page是之前显示过的，则把该Page之后的Page都给移除掉
+                int targetIndex = this.navigateQueue.IndexOf(viewType);
+                if (this.navigateQueue.Count > targetIndex + 1)
+                {
+                    this.navigateQueue.RemoveRange(targetIndex + 1, this.navigateQueue.Count - (targetIndex + 1));
+                }
+            }
+
+            viewInstance.gameObject.SetActive(true);
+        }
+
+        private void SetPageEnable(Type type)
+        {
+            this.HideDependencyWindow(type);
+        }
+
+        /// <summary>
+        /// 将依附于当前Page的View都隐藏掉
+        /// </summary>
+        /// <param name="viewType"></param>
+        private void HideDependencyWindow(Type viewType)
+        {
+            var page = this.uiInstances[viewType];
+
+            List<UIInstanceInfo> hideInfo = new List<UIInstanceInfo>();
+            for (int i = 0, count = this.uiInstanceLoadOrderList.Count; i < count; ++i)
+            {
+                var uiInfo = this.uiInstanceLoadOrderList[i];
+                if (uiInfo.ViewInstance.BasedPage == page.ViewName)
+                {
+                    hideInfo.Add(uiInfo);
+                }
+            }
+
+            for (int i = 0, count = hideInfo.Count; i < count; ++i)
+            {
+                hideInfo[i].ViewInstance.Hide();
+            }
         }
         #endregion
     }
