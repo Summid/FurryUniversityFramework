6b32faf8650f33f1bae5b12ae69401a04d7ee402
Summid <summid@foxmail.com>
2023-01-03 17:12:52 +0800

    整点AudioManager底层逻辑更新

--- FurryUniversity/Assets/Scripts/Core/Audio/AudioLoader.cs
+++ FurryUniversity/Assets/Scripts/Core/Audio/AudioLoader.cs
@@ -1,8 +1,12 @@
+using SFramework.Core.GameManagers;
+using SFramework.Threading.Tasks;
+using SFramework.Utilities;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SFramework.Core.Audio
 {
-    public class AudioLoader : IAudioLoader
+    public class AudioLoader : IAudioLoaderAsync
     {
         public enum AudioAssetType
         {
@@ -14,9 +18,127 @@ namespace SFramework.Core.Audio
             CommonSFX
         }
 
-        public AudioClip LoadAudioClip(string assetName)
+        private readonly BGMAssetLoader bgmLoader = new BGMAssetLoader();
+        private readonly CommonSFXLoader commonSFXLoader = new CommonSFXLoader();
+        private readonly SFXGroupAssetLoader sfxGroupAssetLoader = new SFXGroupAssetLoader();
+
+        public AudioLoader() { }
+
+        public async STask Init()
         {
-            throw new System.NotImplementedException();
+            //先把常驻音效加载进来
+            await this.commonSFXLoader.LoadCommonSFXBundleAsync();
+        }
+
+        public async STask<AudioClip> LoadAudioClipAsync(string assetName)
+        {
+            if (string.IsNullOrEmpty(assetName))
+                return default;
+
+            var type = this.GetAudioAssetType(assetName);
+            switch (type)
+            {
+                case AudioAssetType.BGM:
+                    return await this.bgmLoader.LoadAudioClipAsync(assetName);
+                case AudioAssetType.SFXGroup:
+                    return await this.sfxGroupAssetLoader.LoadAudioClipAsync(assetName);
+                case AudioAssetType.CommonSFX:
+                    return await this.commonSFXLoader.LoadAudioClipAsync(assetName);
+                default:
+                    throw new System.Exception("不可能出现的bug");
+            }
+        }
+
+        private AudioAssetType GetAudioAssetType(string assetName)
+        {
+            if (AudioAssetList.audioAssetInfo.TryGetValue(assetName, out var type))
+            {
+                return type;
+            }
+
+            return AudioAssetType.SFXGroup;
+        }
+
+        public async STask DisposeGroupSFXGroupBundlesAsync()
+        {
+            await this.sfxGroupAssetLoader.DisposeLoadedGroups();
+        }
+
+        private class BGMAssetLoader
+        {
+            private string lastBGMBundleName;
+
+            public async STask<AudioClip> LoadAudioClipAsync(string assetName)
+            {
+                if (string.IsNullOrEmpty(assetName))
+                    return default;
+
+                if (this.lastBGMBundleName != null)
+                {
+                    await AssetBundleManager.UnloadAssetBundleAsync(this.lastBGMBundleName);
+                    this.lastBGMBundleName = null;
+                }
+
+                string bundleName = assetName + StaticVariables.AudioBGMBundleExtension;
+                AudioClip result = await AssetBundleManager.LoadAssetInAssetBundleAsync<AudioClip>(assetName, bundleName);
+                this.lastBGMBundleName = bundleName;
+                return result;
+            }
+        }
+
+        private class CommonSFXLoader
+        {
+            public async STask LoadCommonSFXBundleAsync()
+            {
+                await AssetBundleManager.LoadAssetBundleAsync(StaticVariables.AudioCommonSFXBundleName);
+            }
+
+            public async STask<AudioClip> LoadAudioClipAsync(string assetName)
+            {
+                if (string.IsNullOrEmpty(assetName))
+                    return default;
+
+                return await AssetBundleManager.LoadAssetInAssetBundleAsync<AudioClip>(assetName, StaticVariables.AudioCommonSFXBundleName);
+            }
+        }
+
+        private class SFXGroupAssetLoader
+        {
+            private HashSet<string> loadedBundles = new HashSet<string>();
+
+            public async STask<AudioClip> LoadAudioClipAsync(string assetName)
+            {
+                string bundleName = this.GetGroupBundleName(assetName);
+                if (bundleName == null)
+                    return default;
+
+                if (!this.loadedBundles.Contains(bundleName))
+                {
+                    await AssetBundleManager.LoadAssetBundleAsync(bundleName);
+                    this.loadedBundles.Add(bundleName);
+                }
+
+                return await AssetBundleManager.LoadAssetInAssetBundleAsync<AudioClip>(assetName, bundleName);
+            }
+
+            public async STask DisposeLoadedGroups()
+            {
+                foreach (string bundleName in this.loadedBundles)
+                {
+                    await AssetBundleManager.UnloadAssetBundleAsync(bundleName);
+                }
+                this.loadedBundles.Clear();
+            }
+
+            private string GetGroupBundleName(string assetName)
+            {
+                if (AudioAssetList.sfxGroupAssetInfo.TryGetValue(assetName, out string value))
+                {
+                    return value;
+                }
+
+                return null;
+            }
         }
     }
 }
--- FurryUniversity/Assets/Scripts/Core/Audio/AudioManagerCore.cs
+++ FurryUniversity/Assets/Scripts/Core/Audio/AudioManagerCore.cs
@@ -1,14 +1,163 @@
+using SFramework.Threading.Tasks;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SFramework.Core.Audio
 {
-    public interface IAudioLoader
+    public interface IAudioLoaderAsync
     {
-        AudioClip LoadAudioClip(string assetName);
+        STask<AudioClip> LoadAudioClipAsync(string assetName);
+    }
+
+    public class AudioChannel
+    {
+        public enum Type
+        {
+            Single,
+            Multiple
+        }
+
+        private float volume = 1f;
+        private readonly List<AudioSource> source = new List<AudioSource>();
+
+        private readonly GameObject parent;
+        private readonly IAudioLoaderAsync loader;
+        private readonly Type type;
+
+        public float Volme
+        {
+            get => this.volume;
+            set
+            {
+                this.volume = Mathf.Clamp01(value);
+                this.source.ForEach(s => s.volume = this.volume);
+            }
+        }
+
+        public AudioChannel(GameObject parent, IAudioLoaderAsync loader, Type type)
+        {
+            this.parent = parent;
+            this.loader = loader;
+            this.type = type;
+        }
+
+        public async STask<AudioSource> PlayAsync(string clipName,bool loop = false)
+        {
+            AudioClip clip = await this.loader.LoadAudioClipAsync(clipName);
+            if (clip == null)
+                return null;
+
+            AudioSource pickedSource = this.PickAudioSource();
+
+            pickedSource.loop = loop;
+            pickedSource.clip = clip;
+            pickedSource.volume = this.Volme;
+
+            if (loop)
+            {
+                pickedSource.Play();
+            }
+            else
+            {
+                pickedSource.PlayOneShot(clip);//播放完之后自动关闭
+            }
+
+            return pickedSource;
+        }
+
+        public void Pause()
+        {
+            this.source.ForEach(s => s.Pause());
+        }
+
+        public void Resume()
+        {
+            this.source.ForEach(s => s.Play());
+        }
+
+        public void Stop()
+        {
+            this.source.ForEach(s => s.Stop());
+        }
+
+        public void Mute()
+        {
+            this.Volme = 0f;
+        }
+
+        private AudioSource PickAudioSource()
+        {
+            if (this.type == Type.Single)
+            {
+                if (this.source.Count == 0)
+                    this.source.Add(this.parent.AddComponent<AudioSource>());
+
+                return this.source[0];//Single类型的音效(bgm)只有一个AudioSource
+            }
+            else
+            {
+                //找一个空闲的AudioSource来播放AudioClip
+                AudioSource idleSource = this.source.Find(s => !s.isPlaying);
+
+                if (idleSource == null)
+                {
+                    idleSource = this.parent.AddComponent<AudioSource>();
+                    this.source.Add(idleSource);
+                }
+                else
+                {
+                    idleSource.pitch = 1;
+                }
+
+                return idleSource;
+            }
+        }
     }
 
     public static class AudioManagerCore
     {
+        private static GameObject instance;
+        private static AudioListener listener;
+        private static AudioChannel bgm;
+        private static AudioChannel sfx;
 
+        public static void Init(IAudioLoaderAsync audioLoader)
+        {
+            if (instance != null)
+                return;
+
+            instance = new GameObject("[AudioManager]");
+            listener = instance.AddComponent<AudioListener>();
+
+            Object.DontDestroyOnLoad(instance);
+
+            bgm = new AudioChannel(instance, audioLoader, AudioChannel.Type.Single);
+            sfx = new AudioChannel(instance, audioLoader, AudioChannel.Type.Multiple);
+        }
+
+        public static async STask<AudioSource> PlaySoundAsync(this string assetName, bool loop = false)
+        {
+            return await sfx.PlayAsync(assetName, loop);
+        }
+
+        public static async STask<AudioSource> PlayBGMAsync(this string assetName)
+        {
+            return await bgm.PlayAsync(assetName, true);//bgm默认循环播放
+        }
+
+        public static void PauseBGM()
+        {
+            bgm.Pause();
+        }
+
+        public static void SetSFXVolume(float volume)
+        {
+            sfx.Volme = volume;
+        }
+
+        public static void SetBGMVolume(float volume)
+        {
+            bgm.Volme = volume;
+        }
     }
 }
