502d785c9d0aea9b7e610ec820f92f15001e1d62
Summid <summid@foxmail.com>
2023-01-05 17:34:59 +0800

    浅测一下对话系统；看不懂以前写的代码了，尬住

--- FurryUniversity/Assets/Components/SDialogueSystem/Scripts/SDSDialogue.cs
+++ FurryUniversity/Assets/Components/SDialogueSystem/Scripts/SDSDialogue.cs
@@ -19,5 +19,25 @@ namespace SDS
         //Indexes
         [SerializeField] private int selectedDialogueGroupIndex;
         [SerializeField] private int selectedDialogueIndex;
+
+        public SDSDialogueContainerSO DialogueContainer { get => this.dialogueContainer; }
+
+        public SDSDialogueSO CurrentDialogue => this.dialogue;
+
+        public bool SwitchToNextDialogue()
+        {
+            if (this.dialogue == null)
+                return false;
+            if (this.dialogue.DialogueType != Enumerations.SDSDialogueType.SingleChoice)
+            {
+                Debug.LogWarning($"{this.dialogue.DialogueName} 不是单选对话，不能直接切换到下一句");
+                return false;
+            }
+
+            List<Data.SDSDialogueChoiceData> choice = this.dialogue.Choices;
+            //TODO 判断choice是否合法并切换到下一句
+
+            return false;
+        }
     }
 }
--- FurryUniversity/Assets/Scripts/Core/Init.cs
+++ FurryUniversity/Assets/Scripts/Core/Init.cs
@@ -1,3 +1,5 @@
+using SDS;
+using System.Text;
 using UnityEngine;
 
 namespace SFramework.Core.GameManagers
@@ -11,5 +13,73 @@ namespace SFramework.Core.GameManagers
 
             DontDestroyOnLoad(this.gameObject);
         }
+
+#if UNITY_EDITOR
+        private SDSDialogue dialogue;
+        private StringBuilder showInfos = new StringBuilder();
+        private Vector2 scrollPos;
+        private int line = 0;
+        private void OnGUI()
+        {
+            this.scrollPos = GUILayout.BeginScrollView(this.scrollPos, GUILayout.Width(1000), GUILayout.Height(300));
+            GUILayout.TextArea(this.showInfos.ToString(), new GUIStyle("textArea"), GUILayout.Width(1000), GUILayout.MinHeight(300), GUILayout.Height(20 * this.line));
+            GUILayout.EndScrollView();
+
+            GUILayout.Space(20);
+
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("清除信息"))
+                {
+                    this.showInfos.Clear();
+                }
+
+                if (GUILayout.Button("加载对话文件"))
+                {
+                    this.dialogue = GameObject.Find("DialoguesContainer").GetComponent<SDSDialogue>();
+                    if (this.dialogue != null)
+                    {
+                        this.showInfos.AppendLine($"加载 {this.dialogue} 成功");
+                    }
+                    else
+                    {
+                        this.showInfos.AppendLine($"加载失败");
+                    }
+                    this.line++;
+                }
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            {
+                if (this.dialogue != null)
+                {
+                    if (GUILayout.Button("显示当前对话"))
+                    {
+                        this.showInfos.AppendLine(this.dialogue.CurrentDialogue.Text);
+                        this.line++;
+                    }
+
+                    switch (this.dialogue.CurrentDialogue.DialogueType)
+                    {
+                        case SDS.Enumerations.SDSDialogueType.SingleChoice:
+                            if (GUILayout.Button("切换到下一句"))
+                            {
+
+                            }
+                            break;
+                        case SDS.Enumerations.SDSDialogueType.MultipleChoice:
+                            foreach (var choice in this.dialogue.CurrentDialogue.Choices)
+                            {
+
+                            }
+                            break;
+                    }
+                }
+
+            }
+            GUILayout.EndHorizontal();
+        }
+#endif
     }
 }
