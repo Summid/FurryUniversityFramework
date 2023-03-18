1445efc276e48d3c718f0672a3c1f139f436c538
Summid <summid@foxmail.com>
2023-03-18 16:27:55 +0800

    星期一再努力

---
+++ FurryUniversity/Assets/Scripts/UIObjects/Gen/SettingsView.g.cs
@@ -0,0 +1,15 @@
+//自动生成的代码,根据UIView对应的Prefab脚本ReferenceCollector指定的节点清单自动生成
+using System.Collections.Generic;
+
+namespace SFramework.Core.UI
+{
+    public partial class SettingsView
+    {
+        [UIFieldInit("EnvToggle")]
+        public UnityEngine.UI.Toggle EnvToggle;
+        [UIFieldInit("AudioToggle")]
+        public UnityEngine.UI.Toggle AudioToggle;
+
+    }
+}
+    
---
+++ FurryUniversity/Assets/Scripts/UIObjects/UIView/SettingsView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SFramework.Core.UI
+{
+    [UIView("SettingsView", EnumUIType.Page)]
+    public partial class SettingsView : UIViewBase
+    {
+        
+    }
+}
